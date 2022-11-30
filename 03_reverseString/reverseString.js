63a09e4b74ca1b459f7734177aa47efd3d968c92
Mariutti <pedromariutti@gmail.com>
2022-11-30 19:23:02 -0300

    Exercise 03 DONE

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const stringToArray = string.split("");
+    const arrayReverse = stringToArray.reverse();
+    const arrayToString = arrayReverse.toString();
+    const stringReverse = arrayToString.replaceAll(",","");
+    return stringReverse
 };
 
 // Do not edit below this line
