b24e77623978a91f2b57ab2aa4201142a78b3d49
Mariutti <pedromariutti@gmail.com>
2022-12-03 12:06:25 -0300

    Done exercise 05 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+    const array = Array.from(args);
+    let sum = 0;
+    let arrayTemp;
+    if (array[0] < 0 || array[array.length-1] < 0 ||
+         (typeof array[0] != 'number') || (typeof array[array.length-1] != 'number')){
+        return 'ERROR'
+    }
+    
+    
+        
 
-};
+    if(array[0] > array[array.length-1]){
+        arrayTemp = array[0];
+        array[0] = array[array.length-1];
+        array[array.length-1] = arrayTemp
+    }
+    for (let i = array[0]; i <= array[array.length -1] ; i++){
+        sum += i
+    }
+    return sum
+}
 
 // Do not edit below this line
 module.exports = sumAll;
