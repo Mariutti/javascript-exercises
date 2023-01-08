b9deb6598406317530c5d0090b118213d7ba0f66
Mariutti <pedromariutti@gmail.com>
2023-01-08 13:54:51 -0300

    Solved exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let array = Array.from(string.toLowerCase());
+    let stringTest = '';
+    for(let i = 0; i <= array.length -1; i++){
+        if(array[i] == " " || array[i] == "," || array[i]== "." || array[i] == "!" || array[i] == "?"){
+            continue;
+        } else {
+            stringTest += array[i]
+        }
+    }
+    let stringReverse = Array.from(stringTest).reverse().join("")
+    return stringTest == stringReverse
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
