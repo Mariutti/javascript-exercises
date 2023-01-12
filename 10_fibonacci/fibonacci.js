8b012c657feb4a858fc3c697d3a0bd545d67dcf1
Mariutti <pedromariutti@gmail.com>
2023-01-12 18:50:19 -0300

    Solved exercise 10-Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(n) {
+    let a = 0
+    let b = 1
+    let c = 0
+    if(n < 0){
+        return 'OOPS'
+    }
+    for(let i = 0; i <= n; i++) {
+      c = a
+      a = b
+      b = a + c
+      
+  
+    }
+      return c
+  }
 
 // Do not edit below this line
 module.exports = fibonacci;
