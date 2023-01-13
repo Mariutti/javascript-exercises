c36850765725980ca3b6cbd059403c7d412b1d3b
Mariutti <pedromariutti@gmail.com>
2023-01-13 18:08:55 -0300

    Solved exercise 11 - getTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arrayOfPeople) {
+    let ageOfDeathAnt = -Infinity;
+    return older = arrayOfPeople.reduce((ageAtDeath, person)=>{
+    if(!person.yearOfDeath){
+        ageAtDeath = new Date().getFullYear() - person.yearOfBirth
+    } else{
+        ageAtDeath = person.yearOfDeath - person.yearOfBirth
+    }
+    if(ageAtDeath > ageOfDeathAnt){
+        ageOfDeathAnt = ageAtDeath
+        oldest = person
+    }
+    return oldest
+    }, 0)
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
